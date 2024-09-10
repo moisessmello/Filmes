1743fa77000253f0bd9426e34dfade5192726347
moisessmello <moisesmelo70@gmail.com>
2024-09-09 21:56:59 -0300

    Atividade 5 (revisão)

---
+++ src/app/series/[id]/page.js
@@ -0,0 +1,63 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import { useEffect, useState } from "react";
+import apiMovie from "@/app/services/apiMovies";
+import { Col, Row } from "react-bootstrap";
+import Link from "next/link";
+
+export default function Page({ params }) {
+
+    const [serie, setSerie] = useState({})
+    const [atores, setAtores] = useState([])
+
+    useEffect(() => {
+        apiMovie.get(`tv/${params.id}`).then(resultado => {
+            setSerie(resultado.data)
+        })
+
+        apiMovie.get(`tv/${params.id}/credits`).then(resultado => {
+            setAtores(resultado.data.cast)
+        })
+    }, [])
+
+    return (
+        <Pagina titulo={serie.name}>
+            {
+                serie.id &&
+                <Row className="mt-3">
+                    <Col sm={4}>
+                        <img className="img-fluid" src={'https://image.tmdb.org/t/p/w500/' + serie.poster_path} />
+                    </Col>
+                    <Col sm={8}>
+                        <p><b>Título original: </b>{serie.original_name}</p>
+                        <p><b>Popularidade: </b>{serie.popularity}</p>
+                        <p><b>Data de Lançamento: </b>{serie.release_date}</p>
+                        <p><b>Orçamento: </b>{serie.budget}</p>
+                        <p><b>Gêneros: </b>
+                            {serie.genres.map(item => item.name).join(', ')}
+                        </p>
+                        <p><b>Sinopse: </b>{serie.overview}</p>
+                    </Col>
+                    <Col sm={12}>
+                        <h1>Atores</h1>
+                        <Row>
+                            {atores.map(item => (
+                                <Col
+                                    key={item.id}
+                                    title={item.name}
+                                    className="mb-3"
+                                    sm={2}
+                                >
+                                    <Link href={`/atores/${item.id}`}>
+                                        <img className="img-fluid" src={'https://image.tmdb.org/t/p/w500/' + item.profile_path} />
+                                    </Link>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Col>
+                </Row>
+            }
+        </Pagina>
+    )
+}
--- src/app/series/page.js
+++ src/app/series/page.js
@@ -8,27 +8,27 @@ import Link from "next/link";
 
 export default function Page() {
 
-    const [filmes, setFilmes] = useState([])
+    const [series, setSeries] = useState([])
 
     useEffect(() => {
-        apiMovie.get('movie/popular').then(resultado => {
-            setFilmes(resultado.data.results)
+        apiMovie.get('tv/popular').then(resultado => {
+            setSeries(resultado.data.results)
         })
     }, [])
 
     return (
-        <Pagina titulo="Filmes">
+        <Pagina titulo="Séries">
 
             <Row md={3}>
-                {filmes.map(item => (
+                {series.map(item => (
                     <Col key={item.id} className="mt-3">
                         <Card>
                             <Card.Img height={150} variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.backdrop_path} />
                             <Card.Body>
-                                <Card.Title>{item.title}</Card.Title>
-                                <Card.Text>{item.original_title}</Card.Text>
+                                <Card.Title>{item.name}</Card.Title>
+                                <Card.Text>{item.original_name}</Card.Text>
                                 <Card.Text>Popularidade: {item.popularity}</Card.Text>
-                                <Link className="btn btn-danger" href={`/filmes/${item.id}`}>
+                                <Link className="btn btn-danger" href={`/series/${item.id}`}>
                                     Detalhes
                                 </Link>
                             </Card.Body>
@@ -37,7 +37,7 @@ export default function Page() {
                 ))}
             </Row>
 
-            {filmes.map(item => (
+            {series.map(item => (
                 <p>{item.title}</p>
             ))}
 
